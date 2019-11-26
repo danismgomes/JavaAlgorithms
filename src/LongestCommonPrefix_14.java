10ecce63e1901d896228f4d6414c5743a6baf2c0
Danielle <Gomes>
2019-11-26 11:32:29 -0800

    Add LongestCommonPrefix

---
+++ src/LongestCommonPrefix_14.java
@@ -0,0 +1,27 @@
+public class LongestCommonPrefix_14 {
+    public String longestCommonPrefix(String[] strs) {
+
+        if(strs.length==0)return "";
+
+        String solution = "";
+
+        for (int i = 0; i < strs[0].length(); i++) {
+
+            String s = strs[0].substring(i,i+1);
+
+            for(int j = 1; j<strs.length; j++){
+
+                if(strs[j].length()==i)return solution;
+
+                String s2 = strs[j].substring(i,i+1);
+
+                if(!s.equals(s2))return solution;
+            }
+
+            solution+=s;
+
+        }
+
+        return solution;
+    }
+}
