7f25a5e5e48b54c609c37e1f358345bf0d6da6cc
Danielle <Gomes>
2019-11-25 16:45:02 -0800

    Add PalindromeNumber

--- src/PalindromeNumber_9.java
+++ src/PalindromeNumber_9.java
@@ -1,2 +1,18 @@
 public class PalindromeNumber_9 {
+    public static boolean isPalindrome(int x) {
+
+        if (x < 0){
+            return false;
+        }
+
+        String aString = String.valueOf(x);
+
+        for (int i = 0; i < aString.length()/2 ; i++) {
+            if (aString.charAt(i) != aString.charAt(aString.length() - 1 - i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
--- src/main.java
+++ src/main.java
@@ -2,9 +2,10 @@ public class main {
 
     public static void main(String[] args) {
         //2. ReverseInteger Problem
-        System.out.print("Reverse Integer Problem (number : 123)-> ");
-        System.out.println(ReverseInteger_2.reverse(123));
+        //System.out.println(ReverseInteger_2.reverse(123));
 
+        //9. PalindromeNumber: Is Palindrome?
+        //System.out.println(PalindromeNumber_9.isPalindrome(-1234321));
 
     }
 }
