2a79531aa72fb5bd3bbfba01da5494e11afcc02a
Danielle <Gomes>
2019-11-26 10:49:32 -0800

    Add RomanToInteger

---
+++ src/RomantoInteger_13.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomantoInteger_13 {
+    public static int romanToInt(String s) {
+
+        Map<Character,Integer> map = new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+
+        int i = s.length() -  1;
+        int j = s.length() -  2;
+
+        char c = s.charAt(i);
+        int aInt = map.get(c);
+
+        int solution = map.get(s.charAt(i));
+
+        if (i == 0) {
+            return solution;
+        }
+
+        while (j >= 0 && solution > map.get(s.charAt(j))){
+            solution = solution - map.get(s.charAt(j));
+            j--;
+        }
+
+        j++;
+
+        if (j == 0){
+            return solution;
+        }
+
+        return romanToInt(s.substring(0,j)) + solution;
+    }
+}
--- src/main.java
+++ src/main.java
@@ -7,5 +7,10 @@ public class main {
         //9. PalindromeNumber: Is Palindrome?
         //System.out.println(PalindromeNumber_9.isPalindrome(-1234321));
 
+        //13. Roman to Integer
+        System.out.println(RomantoInteger_13.romanToInt("MCMXCIV"));
+        System.out.println(RomantoInteger_13.romanToInt("LVIII"));
+        System.out.println(RomantoInteger_13.romanToInt("IX"));
+
     }
 }
